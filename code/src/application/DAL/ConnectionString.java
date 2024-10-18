dfec65e35760ef91972cd77b4c64251f6852c898
Dneial <111139207+DRiveraa@users.noreply.github.com>
2024-10-18 11:03:00 -0400

    db
    added connection to the database

---
+++ code/src/application/DAL/ConnectionString.java
@@ -0,0 +1,18 @@
+package application.DAL;
+
+/**
+ * Connection string
+ * @author drivera
+ * @verison 1.0
+ */
+public class ConnectionString {
+	
+	private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String HOST = "cs-dblab01.uwg.westga.edu";
+    private static final String DATABASE = "cs3230f24b";
+    private static final int PORT = 3306;
+
+    public static final String CONNECTION_STRING = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s", HOST, PORT, DATABASE, USERNAME, PASSWORD);
+
+}
--- code/src/application/Main.java
+++ code/src/application/Main.java
@@ -1,34 +1,50 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import application.DAL.ConnectionString;
 
 public class Main extends Application {
-	
-	public static final String WINDOW_TITLE = "MystiMedicare";
-	public static final String LOGIN_WINDOW = "view/LoginPage.fxml";
-	
-	
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent parent = FXMLLoader.load(getClass().getResource(Main.LOGIN_WINDOW));
-			Scene scene = new Scene(parent);
-			primaryStage.setTitle(WINDOW_TITLE);
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(false);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    public static final String WINDOW_TITLE = "MystiMedicare";
+    public static final String LOGIN_WINDOW = "view/LoginPage.fxml";
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            if (checkDatabaseConnection()) {
+                System.out.println("Connected to the database successfully.");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+
+            Parent parent = FXMLLoader.load(getClass().getResource(Main.LOGIN_WINDOW));
+            Scene scene = new Scene(parent);
+            primaryStage.setTitle(WINDOW_TITLE);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkDatabaseConnection() {
+        try (Connection conn = DriverManager.getConnection(ConnectionString.CONNECTION_STRING)) {
+            return conn != null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
