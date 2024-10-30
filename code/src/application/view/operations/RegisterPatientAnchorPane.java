054d2f73c7cd1ead355ea9e38d2f877ca0a5f6bd
Jeffrey <jeffreygaineswrk@gmail.com>
2024-10-30 13:28:43 -0400

    Added username label and full name label

--- code/src/application/view/operations/RegisterPatientAnchorPane.java
+++ code/src/application/view/operations/RegisterPatientAnchorPane.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
+import application.model.credentials.ActiveUser;
 import application.viewModel.operations.RegisterPatientAnchorPaneViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +14,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
@@ -95,6 +97,8 @@ public class RegisterPatientAnchorPane {
 			error.getMessage();
 		}
 	}
+	
+	
 
 	private void setUpGenderComboBox() {
 		this.genderComboBox.getItems().add("Male");
