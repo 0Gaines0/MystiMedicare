8d74c70a16ae7b416791a5320237959d4c5259f6
Gaines <Jeffreygaineswrk@gmail.com>
2024-10-30 22:13:01 -0400

    Implemented fixes for ITR 2

--- code/src/application/view/operations/RegisterPatientAnchorPane.java
+++ code/src/application/view/operations/RegisterPatientAnchorPane.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
-import application.model.credentials.ActiveUser;
 import application.viewModel.operations.RegisterPatientAnchorPaneViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +13,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
