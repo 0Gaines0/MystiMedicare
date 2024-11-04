a86c334c2d86ba6cb355824ee271a2daa88c631f
Dneial <111139207+DRiveraa@users.noreply.github.com>
2024-11-03 21:11:27 -0500

    logout

--- code/src/application/DAL/VisitDAL.java
+++ code/src/application/DAL/VisitDAL.java
@@ -7,7 +7,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class VisitDAL {
 
